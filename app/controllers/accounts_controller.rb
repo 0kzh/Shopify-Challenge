eb9d27bb993e753dfde48e0dbd37b938c3bafac4
Kelvin Zhang <me@kelvinzhang.ca>
2020-09-06 11:54:07 -0700

    Style login/signup, add navbar

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,9 @@
+class AccountsController < ApplicationController
+    def index
+        # user feed
+    end
+
+    def show
+        # user profile
+    end
+end
--- app/javascript/components/Home.tsx
+++ app/javascript/components/Home.tsx
@@ -1,17 +1,65 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React from "react";
+import {
+    Button,
+    Input,
+    Navbar,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+} from "reactstrap";
+
+const logo = require("../../assets/images/logo.svg");
 
 interface Props {}
 
 function Home(props: Props) {
-    const {} = props
+    const {} = props;
+
+    const preventAction = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+    };
 
     return (
         <div>
-            <Link to="/accounts/sign_up">Sign up</Link>
-            <Link to="/accounts/log_in">Log In</Link>
+            <Navbar>
+                <NavbarBrand href="/">
+                    <Nav style={styles.vCenter}>
+                        <NavItem>
+                            <img src={logo} style={styles.logo} />
+                        </NavItem>
+                        <NavItem style={styles.title}>aperture</NavItem>
+                        <NavItem onClick={preventAction}>
+                            <Input type="text" placeholder="Search images..." />
+                        </NavItem>
+                    </Nav>
+                </NavbarBrand>
+                <Nav style={styles.vCenter}>
+                    <NavLink href="/accounts/sign_in">Log in</NavLink>
+                    <NavLink href="/accounts/sign_up">
+                        <Button color="primary">Join for free</Button>
+                    </NavLink>
+                </Nav>
+            </Navbar>
         </div>
-    )
+    );
 }
 
-export default Home
+const styles = {
+    logo: {
+        height: 24,
+        width: 24,
+    },
+    vCenter: {
+        display: "flex",
+        alignItems: "center",
+    },
+    title: {
+        color: "black",
+        marginLeft: 10,
+        marginRight: 30,
+    },
+};
+
+export default Home;
